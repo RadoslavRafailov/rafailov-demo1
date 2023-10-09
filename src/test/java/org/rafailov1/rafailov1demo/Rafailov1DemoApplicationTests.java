3272dc01ba073c9f5c10f207ae2b9ce30f6ced22
RadoslavRafailov <radorafailov1@gmail.com>
2023-10-09 15:09:49 +0300

    gogo

---
+++ src/test/java/org/rafailov1/rafailov1demo/Rafailov1DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rafailov1.rafailov1demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rafailov1DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
